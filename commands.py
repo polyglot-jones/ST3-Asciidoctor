31b77f7e6bbd07cc823492df3af297b3395731be
Craig Jones <craig@k6nnl.com>
2021-07-28 00:41:29 -0700

    Added Gather Dropped Images command

--- commands.py
+++ commands.py
@@ -259,3 +259,31 @@ class AsciidocProseFixupCommand(TextCommand):
 
         self._update_file(txt)
 
+
+class AsciidocGatherDroppedImagesCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self._edit = edit
+        filenames = self._gather_images()
+        results = ["image::{}[]".format(f) for f in filenames]
+        self._replace_selected_text(results)
+        self._unselect()
+
+    def _replace_selected_text(self, doc):
+        if type(doc) is list:
+            doc = "\n".join(doc)
+        doc += "\n"
+        self.view.replace(self._edit, self.view.sel()[0], doc)
+
+    def _unselect(self):
+        s = self.view.sel()
+        pt = s[0].end()
+        s.clear()
+        s.add(sublime.Region(pt,pt))
+
+    def _gather_images(self) -> list:
+        results = []
+        for sheet in self.view.window().sheets():
+            if (type(sheet) is sublime.ImageSheet):
+                results.append(sheet.file_name())
+                sheet.close()
+        return results
